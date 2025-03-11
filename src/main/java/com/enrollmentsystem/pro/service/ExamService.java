0c9d87b02f4b028a3bd2645edb299d8191dddc5d
Kirstin <146112540+Kirstenumali@users.noreply.github.com>
2025-03-11 16:58:39 +0400

    I have a new function called inputNumber

--- src/main/java/com/enrollmentsystem/pro/service/ExamService.java
+++ src/main/java/com/enrollmentsystem/pro/service/ExamService.java
@@ -1,33 +1,57 @@
 package com.enrollmentsystem.pro.service;
 
+import java.util.Scanner;
+
 public class ExamService {
 
     // odd numbers, 1, 3, 5
-    public void oddNumber(int number) {
+    private void oddNumber(int number) {
         for (int i = 3; i < number; i+=2) {
             System.out.println(i);
         }
     }
     // negative number
-    public void negativeNumber(int negativeNumber) {
+    private void negativeNumber(int negativeNumber) {
         for (int i = 0; i <= negativeNumber; i++) {
             System.out.println(-i);
         }
     }
     // odd number with 4
-    public void oddNumberWithFour(int number) {
+    private void oddNumberWithFour(int number) {
        for (int i = 1; i <= number; i++) {
            int j = i % 2;
            if (j == 0) {
-           System.out.println("Even number is: " + i);
-           }
-       }
-        }
+           System.out.println("Even number is: " + i);}}}
 
         // reverse order, 5, 4, 3, 1, 0
-    public void reverseNumber(int number) {
+    private void reverseNumber(int number) {
         for (int i = number; i >= 0; i--) {
             System.out.println(i);
         }
     }
-}
+//    public void inputNumber(int number, int num, int third, int fourth) {
+//        System.out.println("Input value=" + number);
+//        for (int i = 1; i < num; i++) {
+//            System.out.println("*");
+//        }
+//        System.out.println("Input value=" + third);
+//        for (int i = 1; i < fourth; i++) {
+//            System.out.println("*");
+//        }
+
+      public void inputNumber(int number) {
+          System.out.println("Input value=" + number);
+          for (int i = 1; i <= number; i++) {
+              for (int j = 1; j <= i; j++) {
+                  System.out.print("*");
+              }
+              System.out.println("");
+              }
+          for (int i = number - 1; i >= 1; i--) {
+              for (int j = 1; j <= i; j++) {
+                  System.out.print("*");
+              }
+              System.out.println("");
+          }
+          }
+      }
--- src/main/java/com/enrollmentsystem/pro/service/Main.java
+++ src/main/java/com/enrollmentsystem/pro/service/Main.java
@@ -8,18 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        int number = 7;
-//        if (number > 10) {
-//            System.out.println("The number is greater than 10.");
-//        } else if (number > 5) {
-//            System.out.println("The number is greater than 5 but less than or equal to 10.");
-//        } else {
-//            System.out.println("The number is 5 or less.");
-//        }
-        ExamService examService = new ExamService();
-//        examService.oddNumber(10);
-//        examService.negativeNumber(10);
-//        examService.oddNumberWithFour(10);
-        examService.reverseNumber(10);
 
+        ExamService examService = new ExamService();
+        examService.inputNumber(4);
     }}
