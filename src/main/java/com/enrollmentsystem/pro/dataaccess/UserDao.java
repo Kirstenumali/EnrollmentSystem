bd5ad3d66617e97f00de2ab51881df2c04abc1c5
Kirstin <146112540+Kirstenumali@users.noreply.github.com>
2025-03-19 16:07:18 +0400

    I am getting an error for username.

---
+++ src/main/java/com/enrollmentsystem/pro/dataaccess/UserDao.java
@@ -0,0 +1,41 @@
+package com.enrollmentsystem.pro.dataaccess;
+
+import com.enrollmentsystem.pro.User;
+import java.util.ArrayList;
+
+public class UserDao {
+
+    // database
+    private ArrayList<User> userDatabase;
+    // constructor
+    public UserDao() {
+        userDatabase = new ArrayList<>();
+    }
+
+    public ArrayList getAllUser() {
+
+        User user1 = new User();
+        user1.setName("John");
+        userDatabase.add(user1);
+
+        User user2 = new User();
+        user2.setName("Jane");
+        userDatabase.add(user2);
+
+        User user3 = new User();
+        user3.setName("Bob");
+        userDatabase.add(user3);
+
+        System.out.println("All registered users have been created");
+
+        return userDatabase;
+    }
+
+    public ArrayList addUser() {
+
+        User user1 = new User();
+        user1.getName();
+
+        return userDatabase;
+    }
+}
