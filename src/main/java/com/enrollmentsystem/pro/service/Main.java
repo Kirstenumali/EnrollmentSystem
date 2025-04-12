f0de2c63f106acb0ab83f46779f34ca816d9cc7c
Kirstin <146112540+Kirstenumali@users.noreply.github.com>
2025-04-12 20:47:13 +0400

    My last commit in exercise

--- src/main/java/com/enrollmentsystem/pro/service/Main.java
+++ src/main/java/com/enrollmentsystem/pro/service/Main.java
@@ -1,9 +1,6 @@
 package com.enrollmentsystem.pro.service;
 import com.enrollmentsystem.pro.Solution;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
 
     public static void main(String[] args) {
