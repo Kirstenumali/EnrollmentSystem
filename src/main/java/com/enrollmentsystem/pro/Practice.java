d17f281d09cd1b818f39c050b10c9a835c406e1e
Kirstin <146112540+Kirstenumali@users.noreply.github.com>
2025-05-27 09:22:48 +0400

    working with generic class

--- src/main/java/com/enrollmentsystem/pro/Practice.java
+++ src/main/java/com/enrollmentsystem/pro/Practice.java
@@ -9,9 +9,17 @@ public class Practice {
     }
 
     public void checkArray(ArrayList numbers) {
-        int number = (int) numbers.get(0);
+        int size = numbers.size();
+
+        for (int i = 0; i < size; i++) {
+        int number = (int) numbers.get(i);
+
         if (number % 5 == 0) {
             System.out.println(number);
+        }
+        if (i == size -1) {
+            break;
+        }
         }
     }
 }
--- src/main/java/com/enrollmentsystem/pro/service/Main.java
+++ src/main/java/com/enrollmentsystem/pro/service/Main.java
@@ -1,15 +1,31 @@
 package com.enrollmentsystem.pro.service;
 
 import com.enrollmentsystem.pro.Practice;
+import com.enrollmentsystem.pro.Teacher;
 
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Practice practice = new Practice();
-        ArrayList arrayList = new ArrayList();
-        arrayList.add(5);
-        arrayList.add(10);
-        practice.checkArray(arrayList);
+//        Practice practice = new Practice();
+//        ArrayList arrayList = new ArrayList();
+//        arrayList.add(5);
+//        arrayList.add(10);
+//        arrayList.add(20);
+//        arrayList.add(13);
+//        arrayList.add(35);
+//        arrayList.add(50);
+//        practice.checkArray(arrayList);
+
+        Teacher<Integer> teacher = new Teacher();
+        teacher.setName("Niloo");
+        teacher.setId(1223);
+
+        Teacher<String> newTeacher = new Teacher();
+        newTeacher.setName("Kirsten");
+        newTeacher.setId("1332");
+
+        ArrayList<Integer> arrayList = new ArrayList();
+        arrayList.add(7665);
     }}
